84112d5ec5f902caff4a54cae7e1f5c2bc0e146c
Issamuhammad <issamuhammad891@gmail.com>
2025-07-07 03:01:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Lucky Tech Hub Bot",
   botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255655750895', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
